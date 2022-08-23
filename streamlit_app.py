02009c6311134e99c12a00350890ea073c8f14cf
Plamen Rabadzhiyski <plamen@pmtonomy.com>
2022-08-23 19:19:06 +0300

    feat: add pd data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("My Mom's New Healthy Dinner")
 
@@ -8,3 +9,9 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Add data
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Display data
+streamlit.dataframe(my_fruit_list)
