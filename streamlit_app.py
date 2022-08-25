0df5c2c7993ff94993e1fd1839488a3124467286
Plamen Rabadzhiyski <plamen@pmtonomy.com>
2022-08-25 16:50:27 +0300

    bug: fix pandas spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response) # writes the data to the screen
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
